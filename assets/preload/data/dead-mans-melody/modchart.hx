b5a8b5920e89230e00f8d98606691d70df133235
Rifxii <73841096+rifxii@users.noreply.github.com>
2023-01-17 16:20:30 -0500

    tea is now ghostphobic

--- assets/preload/data/dead-mans-melody/modchart.hx
+++ assets/preload/data/dead-mans-melody/modchart.hx
@@ -1,8 +1,16 @@
 import Character;
 import PlayState;
+import flixel.effects.FlxFlicker;
 import flixel.tweens.FlxEase;
 import flixel.util.FlxTimer;
 
+// TEA FLIPPED = LOOKING RIGHT
+var platform:FlxSprite;
+var tea:FlxSprite;
+var teaFlipped:Bool = false;
+var ghosts = [];
+
+//
 var pasta:Character;
 var isDad:Bool = false;
 var camTween:FlxTween;
@@ -10,6 +18,15 @@ var rowProperties = [];
 
 function onCreate()
 {
+	platform = new FlxSprite().loadGraphic(Paths.image("paste/platform"));
+	platform.antialiasing = true;
+	platform.visible = false;
+	add(platform);
+
+	tea = new Character(0, 0, "gf-fight");
+	tea.visible = false;
+	add(tea);
+
 	pasta = new Character(dad.x - 550, dad.y - 580, "toothpaste-mad", false);
 	add(pasta);
 	pasta.visible = false;
@@ -52,11 +69,32 @@ function onUpdate(elapsed:Float)
 {
 	var cB = (Conductor.songPosition / 1000) * (Conductor.bpm / 60);
 	pasta.y = (dad.y - 580) + (11 * Math.cos((cB / 2) * Math.PI));
+
+	for (i in ghosts)
+	{
+		if (i.animation.curAnim.name == "idle"
+			&& i.x >= tea.x - 160
+			|| i.animation.curAnim.name == "idle-flip"
+			&& i.x <= tea.x + tea.width)
+		{
+			teaPunch(i);
+		}
+	}
 }
 
 function onMoveCamera(dad:Bool)
 {
 	isDad = dad;
+}
+
+function onBeatHit(curBeat:Int)
+{
+	var idleAnim = getTeaIdle();
+	if (tea.animation.curAnim.name != idleAnim && tea.animation.finished || tea.animation.curAnim.name == idleAnim)
+		tea.playAnim(idleAnim);
+
+	if (curBeat >= 146 && curBeat % 3 == 0 && FlxG.random.bool(40))
+		spawnGhost();
 }
 
 function onStepHit(curStep:Int)
@@ -77,8 +115,14 @@ function onStepHit(curStep:Int)
 			gf.visible = false;
 			dad.visible = false;
 			pasta.visible = true;
+
+			tea.visible = true;
+			platform.visible = true;
+
 			boyfriend.setPosition(770, 225);
 			DAD_CAM_OFFSET.y -= 175;
+			tea.setPosition(boyfriend.x + 690, boyfriend.y - 500);
+			platform.setPosition(tea.x - 28, tea.y + tea.height - 125);
 
 			if (camTween != null)
 				camTween.cancel();
@@ -95,4 +139,72 @@ function setHUDVisibility(theBool:Bool)
 
 	for (i in [iconP1, iconP2, healthBar, healthBarBG, scoreTxt])
 		i.visible = theBool;
+}
+
+function teaTurn(flip)
+{
+	teaFlipped = flip;
+	if (tea.animation.curAnim.name == "idle-flip" && !flip || tea.animation.curAnim.name == "idle" && flip)
+	{
+		var idleAnim = getTeaIdle();
+		tea.playAnim("turn" + (teaFlipped ? "RIGHT" : "LEFT"));
+	}
+}
+
+function teaPunch(ghost)
+{
+	tea.playAnim("punch" + (ghost.ID == 1 ? "-flip" : ""), true);
+
+	ghost.velocity.x = 0;
+	ghost.animation.play("hurt" + (ghost.ID == 1 ? "-flip" : ""), true);
+	ghost.animation.finishCallback = function(a)
+	{
+		ghosts.remove(ghost);
+
+		FlxFlicker.flicker(ghost, 0.36, 0.12, false, false, function(flicker:FlxFlicker)
+		{
+			ghost.kill();
+		});
+
+		if (ghosts[0] != null)
+			teaTurn(ghosts[0].ID == 1);
+	}
+}
+
+function getTeaIdle()
+{
+	return "idle" + (teaFlipped ? "-flip" : "");
+}
+
+function spawnGhost()
+{
+	var ghost = new FlxSprite();
+	ghost.antialiasing = true;
+	ghost.ID = FlxG.random.int(0, 1); // 0 = LEFT GHOST. 1 = RIGHT GHOST
+
+	ghost.frames = Paths.getSparrowAtlas('paste/ghost');
+	ghost.animation.addByPrefix("idle", "ghostoguy instance", 24, true);
+	ghost.animation.addByPrefix("idle-flip", "ghostoguyRIGHT instance", 24, true);
+	ghost.animation.addByPrefix("hurt", "ghostoguyHURT instance", 24, false);
+	ghost.animation.addByPrefix("hurt-flip", "ghostoguyHURTright instance", 24, false);
+	ghost.animation.play("idle" + (ghost.ID == 1 ? "-flip" : ""));
+	ghost.setPosition(ghost.ID == 0 ? tea.x - 900 : tea.x + tea.width + 700, tea.y + 90);
+	ghost.scale.y = ghost.scale.x = FlxG.random.float(0.69, 1);
+
+	// idk why its like this but the first ghost will ALWAYS be invisible no matter when spawned
+	// adding the position parameter seemed to fix it.
+	add(ghost, getIndexOfMember(tea));
+
+	if (ghosts.length <= 0)
+	{
+		teaTurn(ghost.ID == 1);
+	}
+
+	ghosts.push(ghost);
+
+	ghost.velocity.x = FlxG.random.int(590, 1510) * (ghost.ID == 1 ? -1 : 1);
+	ghost.alpha = 0;
+	FlxTween.tween(ghost, {alpha: FlxG.random.float(0.8, 0.94)}, FlxG.random.float(0.3, 0.6));
+
+	trace("ghost create " + ghost);
 }
