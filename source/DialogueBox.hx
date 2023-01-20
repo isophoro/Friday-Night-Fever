f6a94253f03d623c0721d4dadfea336cd43f2f30
isophoro <christopherguerra843@gmail.com>
2023-01-19 22:23:02 -0800

    I DID ITTTT

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -162,6 +162,8 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 		}
 	}
 
+	var daLibrary:String;
+
 	function startDialogue()
 	{
 		if (actions[0] == null)
@@ -172,25 +174,21 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 		var action = actions[0];
 		actions.shift();
 
-		if(PlayState.SONG.song.toLowerCase() == 'shadow')
-		{
-			action.library = 'shadow';
-			trace(action.library);
-		}
-		else
-		{
-			action.library = 'shared';
-			trace(action.library);
-		}
+		trace(daLibrary);
 
 		if (action.playSound != null)
 		{
-			FlxG.sound.play(Paths.sound(action.playSound, action.library), 1);
+			FlxG.sound.play(Paths.sound(action.playSound, daLibrary), 1);
+		}
+
+		if(daLibrary == null)
+		{
+			daLibrary = 'shared';
 		}
 
 		if (action.fadeInMus != null)
 		{
-			FlxG.sound.playMusic(Paths.music(action.fadeInMus, action.library), 0);
+			FlxG.sound.playMusic(Paths.music(action.fadeInMus, daLibrary), 0);
 			FlxG.sound.music.fadeIn(1,0,1);
 		}
 
@@ -209,7 +207,7 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 		{
 			if (action.setBG.length > 0)
 			{
-				bg.loadGraphic(Paths.image(action.setBG, action.library));
+				bg.loadGraphic(Paths.image(action.setBG, daLibrary));
 				bg.alpha = 1;
 			}
 			else
@@ -340,6 +338,9 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 	{
 		dialogueStarted = false;
 
+		if(FlxG.sound.music != null)
+			FlxG.sound.music.fadeOut(1, 0);
+
 		for (i in [curLeft, curRight, box, text, bg])
 		{
 			if (i != null)
@@ -373,24 +374,13 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 		{
 			var action:DialogueAction = {};
 
-			if(PlayState.SONG.song.toLowerCase() == 'shadow') //i gave up im just doing this
-				{
-					action.library = 'shadow';
-					trace(action.library);
-				}
-				else
-				{
-					action.library = 'shared';
-					trace(action.library);
-				}
-
 			if (a.has.portrait || a.has.portraits)
 			{
 				if (a.has.portraits)
 				{
 					var s = a.att.portraits.split(",");
 					for (i in s)
-						addPortrait(i, action.library);
+						addPortrait(i, daLibrary);
 
 					action.portraits = s;
 				}
@@ -398,7 +388,7 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 				{
 					var portrait = a.att.portrait;
 					action.portrait = portrait;
-					addPortrait(portrait, action.library);
+					addPortrait(portrait, daLibrary);
 
 					if (a.has.side)
 						action.side = a.att.side.toLowerCase().charAt(0) == "r" ? RIGHT : LEFT;
@@ -420,6 +410,22 @@ class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 				action.fillBG = FlxColor.fromString(a.att.fillBG);
 			else if (a.has.setBG)
 				action.setBG = a.att.setBG;
+
+			if (a.has.library)
+			{
+				action.library = a.att.library;
+				daLibrary = a.att.library;
+			}
+				
+
+			if (a.has.playSound)
+				action.playSound = a.att.playSound;
+
+			if (a.has.fadeInMus)
+				action.fadeInMus = a.att.fadeInMus;
+
+			if (a.has.fadeOutMus)
+				action.fadeOutMus = a.att.fadeOutMus;
 
 			if (a.has.effect)
 				action.effect = a.att.effect.toLowerCase();
